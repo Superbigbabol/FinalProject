49ba41c318cc0bc998c8324be5c4228e0b040a05
Junqi Hong <harryhong104@gmail.com>
2023-03-19 00:22:33 -0400

    Junqi initial build-1

--- app/src/main/java/algonquin/cst2335/finalproject/Weather.java
+++ app/src/main/java/algonquin/cst2335/finalproject/Weather.java
@@ -1,14 +1,43 @@
 package algonquin.cst2335.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatSpinner;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 
 public class Weather extends AppCompatActivity {
+
+    private AppCompatSpinner mSpinner;
+    private ArrayAdapter<String> mSpAdapter;
+    private  String[] mCities;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
+
+        initView();
+
+    }
+
+    private void initView() {
+        mSpinner = findViewById(R.id.sp_city);
+        mCities = getResources().getStringArray(R.array.cities);
+        mSpAdapter = new ArrayAdapter<>(this,R.layout.sp_item_layout,mCities);
+        mSpinner.setAdapter(mSpAdapter);
+        mSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
 }
