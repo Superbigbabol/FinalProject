c71f0ab61c532c3fb82abdffca3beb4d40aaf52e
Junqi Hong <harryhong104@gmail.com>
2023-03-20 21:09:32 -0400

    Junqi initial build-2

--- app/src/main/java/algonquin/cst2335/finalproject/Weather.java
+++ app/src/main/java/algonquin/cst2335/finalproject/Weather.java
@@ -1,18 +1,42 @@
 package algonquin.cst2335.finalproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatSpinner;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import algonquin.cst2335.finalproject.util.NetUtil;
 
 public class Weather extends AppCompatActivity {
 
     private AppCompatSpinner mSpinner;
     private ArrayAdapter<String> mSpAdapter;
     private  String[] mCities;
+
+    private TextView tvWeather, tvTem,tvTemLowHigh,tvWind;
+    private ImageView ivWeather;
+    private RecyclerView rlvFutureWeather;
+    private Handler mHandler = new Handler(Looper.myLooper()){
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+            if (msg.what==0){
+                String weather = (String) msg.obj;
+                Log.d("fan","--receive data of weather--" + weather);
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +54,10 @@ public class Weather extends AppCompatActivity {
         mSpinner.setAdapter(mSpAdapter);
         mSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+                String selectedCity = mCities[position];
+
+                getWeatherOfCity(selectedCity);
 
             }
 
@@ -39,5 +66,31 @@ public class Weather extends AppCompatActivity {
 
             }
         });
+
+        tvWeather = findViewById(R.id.tv_weather);
+        tvTem = findViewById(R.id.tv_tem);
+        tvTemLowHigh = findViewById((R.id.tv_tem_low_high));
+        tvWind = findViewById(R.id.tv_wind);
+        ivWeather = findViewById(R.id.iv_weather);
+        rlvFutureWeather = findViewById(R.id.rlv_future_weather);
+    }
+
+    private void getWeatherOfCity(String selectedCity) {
+        //connect weather api
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //connect
+                String weatherOfCity = NetUtil.getWeatherOfCity(selectedCity);
+                //use handler to transfer data
+                Message message = Message.obtain();
+                message.what=0;
+                message.obj=weatherOfCity;
+                mHandler.sendMessage(message);
+
+
+
+            }
+        }).start();
     }
 }
---
+++ app/src/main/java/algonquin/cst2335/finalproject/util/NetUtil.java
@@ -0,0 +1,80 @@
+package algonquin.cst2335.finalproject.util;
+
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class NetUtil {
+    public static final String URL_WEATHER_WITH_FUTURE = "http:";
+
+    public static String doGet(String urlStr){
+
+        String result ="";
+        //connect to web
+        HttpURLConnection connection = null;
+        InputStreamReader inputStreamReader = null;
+        BufferedReader bufferedReader = null;
+        try {
+            URL url = new URL(urlStr);
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setConnectTimeout(5000);
+
+            //read data
+            InputStream inputStream = connection.getInputStream();
+            inputStreamReader = new InputStreamReader(inputStream);
+            //binary data to memory
+            bufferedReader = new BufferedReader(inputStreamReader);
+            //read data from memory
+            StringBuilder stringBuilder = new StringBuilder();
+            String line = "";
+            while ((line=bufferedReader.readLine())!=null){
+                stringBuilder.append(line);
+            }
+
+            result = stringBuilder.toString();
+
+
+        }catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            if(connection!=null){
+                connection.disconnect();
+            }
+            if(inputStreamReader != null){
+                try{
+                    inputStreamReader.close();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+            if(bufferedReader!=null){
+                try{
+                    bufferedReader.close();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+
+
+        }
+
+        return result;
+    }
+
+    public static String getWeatherOfCity(String city){
+        String result = "";
+        //combine weather data URL
+        String weatherUrl = URL_WEATHER_WITH_FUTURE + "&city=" + city;
+        Log.d("fan","----weatherUrl----" + weatherUrl);
+
+        String weatherResult =  doGet(weatherUrl);
+        Log.d("fan","----weatherUrl----" + weatherResult);
+        return  weatherResult;
+    }
+}
