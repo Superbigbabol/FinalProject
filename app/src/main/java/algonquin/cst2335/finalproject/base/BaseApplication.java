bf4d54c4cc46272e20993a50a55b7e5248429b84
lucy chen <122634378+luyuchen0788@users.noreply.github.com>
2023-03-22 16:47:06 -0400

    New york time (#4)
    * finish milestone1:1,4,5,6,9,10,11,12
    * ready to merge into master

---
+++ app/src/main/java/algonquin/cst2335/finalproject/base/BaseApplication.java
@@ -0,0 +1,21 @@
+package algonquin.cst2335.finalproject.base;
+
+
+import android.app.Application;
+
+
+public class BaseApplication extends Application {
+    private static BaseApplication app;
+
+    public static BaseApplication getInstance() {
+        return app;
+    }
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        app = this;
+
+    }
+}
---
+++ app/src/main/java/algonquin/cst2335/finalproject/bean/CollectionBean.java
@@ -0,0 +1,3 @@
+package algonquin.cst2335.finalproject.bean;
+public class CollectionBean {
+}
